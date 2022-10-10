e4bcb30c83b0293041833085dd36976686d60749
frantecbh <franciscomenezesbh@gmail.com>
2022-10-10 17:28:46 -0300

    setup projeto

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+
+export function App() {
+ return(
+
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+      <h1>Ola Mundo</h1>
+  </ThemeProvider>
+  
+
+ )
+  
+}
+
+
